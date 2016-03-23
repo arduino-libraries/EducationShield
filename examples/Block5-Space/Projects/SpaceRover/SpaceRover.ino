6ea1afee8c9b7ad79d529a9201dd9aba82554d91
marcus johansson <mj.emeth@gmail.com>
2016-03-23 10:37:43 +0100

    removed the Nova beam for photoshoot

--- examples/Block5-Space/Projects/SpaceRover/SpaceRover.ino
+++ examples/Block5-Space/Projects/SpaceRover/SpaceRover.ino
@@ -1,32 +1,34 @@
 #include <EducationShield.h>
 
-BLEuart uart=BLEuart(TYPE_LOGOROBOT);
+BLEuart uart = BLEuart(TYPE_LOGOROBOT);
 
-Wheels wheels=Wheels(6, 9);
+Wheels wheels = Wheels(6, 9);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   uart.setName("LogoRobot");
   uart.begin();
+  
+ wheels.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(uart.searchCentral()){
+  if (uart.searchCentral()) {
     Serial.println("Connected to central ");
-    while(uart.connected()){
-      if(uart.dataReceived()){
+    while (uart.connected()) {
+      if (uart.dataReceived()) {
         uart.fetchData();
-        
+
         Serial.print("Received number of commands: ");
         Serial.println(uart.getReceivedLength());
-        
-        for(int i=0;i<uart.getReceivedLength();i++){
-          unsigned char n=uart.receivedString()[i];
-          
+
+        for (int i = 0; i < uart.getReceivedLength(); i++) {
+          unsigned char n = uart.receivedString()[i];
+
           Serial.print(n);
-          
+
           logoMove(n);
         }
         Serial.println();
@@ -37,23 +39,23 @@ void loop() {
   }
 }
 
-void logoMove(unsigned char direction){
-  switch(direction){
+void logoMove(unsigned char direction) {
+  switch (direction) {
     case 1:	//forward
       wheels.goForward();
-      delay(3000);
+      delay(750);
       break;
     case 2:	//backwards
       wheels.goBackwards();
-      delay(3000);
+      delay(750);
       break;
     case 3:	//left
       wheels.turnLeft();
-      delay(1500);
+      delay(250);
       break;
     case 4:	//right
       wheels.turnRight();
-      delay(1500);
+      delay(250);
       break;
   }
   wheels.standStill();
