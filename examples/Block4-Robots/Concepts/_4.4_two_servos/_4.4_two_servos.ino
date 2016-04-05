df6dbc840aa464e367ab1fb908e13728c674d773
m-ehrndal <m.ehrndal@arduino.cc>
2016-04-05 11:04:05 +0200

    Updated D5 and D9 to D9 and D6

--- examples/Block4-Robots/Concepts/_4.4_two_servos/_4.4_two_servos.ino
+++ examples/Block4-Robots/Concepts/_4.4_two_servos/_4.4_two_servos.ino
@@ -3,20 +3,20 @@
 Servo myservo1, myservo2;
 
 void setup() {
-  myservo1.attach(9);
-  myservo2.attach(5);
+  myservo1.attach(6);
+  myservo2.attach(9);
 }
 
 void loop() {
   myservo2.detach();
-  myservo1.attach(9);
+  myservo1.attach(6);
   myservo1.write(70);
   delay(1000);
   myservo1.write(120);
   delay(1000);
 
   myservo1.detach();
-  myservo2.attach(5);
+  myservo2.attach(9);
   myservo2.write(70);
   delay(1000);
   myservo2.write(120);
